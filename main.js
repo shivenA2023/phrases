32322ac5c4d863df3a148f3010be1f33886773fa
shivenA2023 <124804613+shivenA2023@users.noreply.github.com>
2023-06-12 13:08:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//Program Title
+console.log("hi");
+
+document.getElementById("btnx").addEventListener("click", displayMessage1);
+
+function displayMessage1() {
+  alert("...bullet");
+}
+
+document.getElementById("btn2").addEventListener("click", displayMessage2);
+
+function displayMessage2() {
+  alert("...never");
+}
+
+document.getElementById("btn3").addEventListener("click", displayMessage3);
+
+function displayMessage3() {
+  alert("...bush");
+}
+
+document.getElementById("btn4").addEventListener("click", displayMessage4);
+
+function displayMessage4() {
+  alert("...wall");
+}
+
+document.getElementById("btn5").addEventListener("click", displayMessage5);
+
+function displayMessage5() {
+  alert("...bridges");
+}
